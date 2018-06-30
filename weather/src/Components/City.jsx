b971bf7e691c1347e11087035a2ace4373f68e10
Xavier <xaviermunroe@ac.c4q.nyc>
2018-06-29 20:02:05 -0400

    added styling

--- weather/src/Components/City.jsx
+++ weather/src/Components/City.jsx
@@ -1,33 +1,53 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export const City = ({ city }) => {
+export const City = ({city}) => {
 
-  let currentTemp = ~~(city.temp * (9/5) - 459.67)
-  let maxTemp = ~~(city.temp_max * (9/5) - 459.67)
-  let minTemp = ~~(city.temp_min * (9/5) - 459.67)
+  let currentTemp = ~~(city.temp * (9 / 5) - 459.67)
+  let isHot = currentTemp > 80
+  let isMild = currentTemp > 55
+  let maxTemp = ~~(city.temp_max * (9 / 5) - 459.67)
+  let minTemp = ~~(city.temp_min * (9 / 5) - 459.67)
   let humidity = city.humidity + '%'
   let sunrise = new Date(city.sunrise * 1000)
   let sunset = new Date(city.sunset * 1000)
-  let sunriseHour = sunrise.getHours() <= 12 ? sunrise.getHours() : sunrise.getHours() - 12
-  let sunsetHour = sunset.getHours() <= 12 ? sunset.getHours() : sunset.getHours() - 12
-  let sunriseMinutes = sunrise.getMinutes() <= 9 ? '0' + sunrise.getMinutes() : sunrise.getMinutes()
-  let sunsetMinutes = sunset.getMinutes() <= 9 ? '0' + sunset.getMinutes() : sunset.getMinutes()
+  let sunriseHour = sunrise.getHours() <= 12
+    ? sunrise.getHours()
+    : sunrise.getHours() - 12
+  let sunsetHour = sunset.getHours() <= 12
+    ? sunset.getHours()
+    : sunset.getHours() - 12
+  let sunriseMinutes = sunrise.getMinutes() <= 9
+    ? '0' + sunrise.getMinutes()
+    : sunrise.getMinutes()
+  let sunsetMinutes = sunset.getMinutes() <= 9
+    ? '0' + sunset.getMinutes()
+    : sunset.getMinutes()
+  let weatherImg = `http://openweathermap.org/img/w/${city.icon}.png`
 
-  return(
-    <div className='city-container'>
-      <div className='city-head'>
-        <div className='back-link'>
-          <Link to='/'>Back to Main Page</Link>
+  return (<div className='city-container'>
+    <div className='city-info-container'>
+      <h1 className='city-name'>{city.city_name}</h1>
+      <div className='city-weather'>
+        <div className='city-weather-image'>
+          <img height='100%' width='100%' src={weatherImg} alt='weather'/>
         </div>
-        <h1 className='city-name'>{city.city_name}</h1>
       </div>
-      <div className='city-info-container'>
-        <p>Currently: {currentTemp + '℉'}</p>
-        <p>Low: {minTemp + '℉'} | High: {maxTemp + '℉'}</p>
+      <span className={isHot
+          ? 'hot'
+          : isMild
+            ? 'mild'
+            : 'cold'}>{currentTemp + '℉'}</span>
+      <div className='weather-info-extended'>
+        <p>Low: {minTemp + '℉'}</p>
+        <p>High: {maxTemp + '℉'}</p>
         <p>Humidity: {humidity}</p>
-        <p>Sunrise: {sunriseHour + ':' + sunriseMinutes + ' AM'} | Sunset: {sunsetHour + ':' + sunsetMinutes + ' PM'}</p>
+        <p>Sunrise: {sunriseHour + ':' + sunriseMinutes + ' AM'}</p>
+        <p>Sunset: {sunsetHour + ':' + sunsetMinutes + ' PM'}</p>
       </div>
     </div>
-  )
+    <div className='back-link'>
+      <Link to='/'>Back to Main Page</Link>
+    </div>
+  </div>)
 }
--- weather/src/Components/Oversight.jsx
+++ weather/src/Components/Oversight.jsx
@@ -11,10 +11,11 @@ export const Oversight = ({cities, time}) => {
   let afternoon = hours >= 12 && hours < 16
   let evening = hours >= 16 && hours < 20
   let night = hours >= 20 || hours < 5
+  let test = 'oversight-header'
   console.log(cities)
   return (<div>
     <div className='oversight-greeting'>
-      <h1 className='oversight-header'>
+      <h1 className={test}>
         {
           morning
             ? 'Good Morning'
@@ -27,21 +28,29 @@ export const Oversight = ({cities, time}) => {
                   : ''
         }
       </h1>
-      <div>
-        {currentDate[0]}
-      </div>
+      {currentDate[0]}
     </div>
     <div className='oversight-city-container'>
       {
         cities.map(city => {
           let path = `/city/${city.city_name}`
-          let currentTemp = ~~(city.temp * (9/5) - 459.67)
+          let currentTemp = ~~(city.temp * (9 / 5) - 459.67)
+          let isHot = currentTemp > 80
+          let isMild = currentTemp > 55
+          let weatherImg = `http://openweathermap.org/img/w/${city.icon}.png`
           return (<div className='city-box'>
             <Link to={path}>
               <h3 className='city-box-header'>{city.city_name}</h3>
             </Link>
-            <p>Current Temperature: {currentTemp}</p>
-            <p>Weather: {city.weather_description}</p>
+            <span className='weather-description'>{city.weather_description}</span>
+            <div className='weather-box'>
+              <img src={weatherImg} alt='weather' className='weather-icon'/>
+            </div>
+            <span className={isHot
+                ? 'hot'
+                : isMild
+                  ? 'mild'
+                  : 'cold'}>{currentTemp + '℉'}</span>
           </div>)
         })
       }
